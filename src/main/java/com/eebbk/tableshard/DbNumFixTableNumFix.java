b982ce38c6ceb946f0ee5e17e80ee6a96d968145
liupengfei <18099538715@163.com>
2017-06-21 20:08:20 +0800

    四个分库分表类公共部分做成一个父类

--- src/main/java/com/eebbk/tableshard/DbNumFixTableNumFix.java
+++ src/main/java/com/eebbk/tableshard/DbNumFixTableNumFix.java
@@ -1,8 +1,5 @@
 package com.eebbk.tableshard;
 
-import org.apache.log4j.Logger;
-import org.aspectj.lang.JoinPoint;
-
 /**
  * @项目名称：DbRegionTableNum
  * @类名称：DbRegionTableNum
@@ -11,72 +8,9 @@ import org.aspectj.lang.JoinPoint;
  * @创建时间：2017年6月21日 下午5:34:33
  * @company:步步高教育电子有限公司
  */
-public class DbNumFixTableNumFix {
-	private Logger log = Logger.getLogger(this.getClass());
-	private String dbPrefix;// 数据源前缀
-	private String tablePrefix;// 表名前缀
-	private Integer dbShardParam;// db分片区间
-	private Integer tableShardParam;// 数据表分片个数
+public class DbNumFixTableNumFix extends AbstractTableDbShard {
 
-	public String getDbPrefix() {
-		return dbPrefix;
-	}
-
-	public String getTablePrefix() {
-		return tablePrefix;
-	}
-
-	public void setDbPrefix(String dbPrefix) {
-		this.dbPrefix = dbPrefix;
-	}
-
-	public void setTablePrefix(String tablePrefix) {
-		this.tablePrefix = tablePrefix;
-	}
-
-	public Integer getDbShardParam() {
-		return dbShardParam;
-	}
-
-	public void setDbShardParam(Integer dbShardParam) {
-		this.dbShardParam = dbShardParam;
-	}
-
-	public Integer getTableShardParam() {
-		return tableShardParam;
-	}
-
-	public void setTableShardParam(Integer tableShardParam) {
-		this.tableShardParam = tableShardParam;
-	}
-
-	/**
-	 * 方法执行之前进行分库分表的设置
-	 * @description
-	 * @author liupengfei
-	 * @date 2016年12月27日 上午10:16:47
-	 * @param jionpoint
-	 */
-
-	public void beforeAdvice(JoinPoint jionpoint) {
-		try {
-			Object[] args = jionpoint.getArgs();
-			if (args[0] instanceof Long || args[0] instanceof Integer || args[0] instanceof String) {
-				setParam(args[0]);
-			} else {
-				Object object = ((ShardId) args[0]).getShardId();
-				setParam(object);
-			}
-		} catch (Exception e) {
-			log.error("分库分表出错", e);
-		}
-	}
-
-	public void afterAdvice() {
-		DatabaseContextHolder.clearShardParam();
-	}
-
-	private void setParam(Object object) {
+	public void setParam(Object object) {
 		Long args = null;
 		if (object instanceof Long || object instanceof Integer) {
 			args = (Long) object;
@@ -85,25 +19,15 @@ public class DbNumFixTableNumFix {
 			args = (long) hashString((String) object);
 		}
 		// 如果不分库
-		if (dbPrefix == null || dbPrefix == "") {
-			DatabaseContextHolder.setTableName(this.tablePrefix + ((Long) args) % tableShardParam);
+		if (getDbPrefix() == null || getDbPrefix() == "") {
+			DatabaseContextHolder.setTableName(getTablePrefix() + ((Long) args) % getTableShardParam());
 			return;
 		}
-		if (tablePrefix == null || tablePrefix == "") {// 如果不分表
-			DatabaseContextHolder.setDbName(this.dbPrefix + ((Long) args) % dbShardParam);
+		if (getTablePrefix() == null || getTablePrefix() == "") {// 如果不分表
+			DatabaseContextHolder.setDbName(this.getDbPrefix() + ((Long) args) % getDbShardParam());
 			return;
 		}
-		DatabaseContextHolder.setShardParam(this.tablePrefix + ((Long) args) % tableShardParam, this.dbPrefix + ((Long) args) % dbShardParam);
+		DatabaseContextHolder.setShardParam(this.getTablePrefix() + ((Long) args) % getTableShardParam(), getDbPrefix() + ((Long) args) % getDbShardParam());
 	}
 
-	/**
-	 * @description
-	 * @author liupengfei 默认的string类型的hash方法
-	 * @date 2017年6月21日 下午3:41:23
-	 * @param s
-	 * @return
-	 */
-	public Integer hashString(String s) {
-		return s.hashCode();
-	}
 }
